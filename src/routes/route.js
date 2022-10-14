1af164dbb48018ab382bacaa1bc65a6b403d1673
soniya437 <ysoniya437@gmail.com>
2022-10-14 16:47:13 +0530

    upgrade logger

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();///test-you
 //importing a custom module
-const xyz = require('../logger')
+const xyz = require('../logger/logger')
 //importing external package
 const underscore = require('underscore')
+
 
 router.get('/test-me', function (req, res) {
     //Calling the components of a different custom module
     console.log("Calling my function ",xyz.myFunction())
+    console.log(xyz.Welcome())
     console.log("The value of the constant is ",xyz.myUrl)
     //Trying to use an external package called underscore
     let myArray = ['Akash', 'Pritesh', 'Sabiha']
