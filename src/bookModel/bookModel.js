230de0626d9fb86828a1979f79beccf48b7e41e6
https://github.com/AdityaKuntal/backen-nodejs.git <akjaat001kuntal@gmail.com>
2022-10-21 23:44:07 +0530

    newchange

--- src/bookModel/bookModel.js
+++ src/bookModel/bookModel.js
@@ -1,2 +1,22 @@
 const mongoose = require('mongoose')
 
+
+
+let bookSchema = new mongoose.Schema({
+ 
+    bookName: String,
+    authorName: String,
+    category: String,
+    year: Number
+
+
+})
+
+
+
+
+
+
+
+module.exports = mongoose.model('Book Model',bookSchema)
+
