1cd2da514290ad562cc2d55abab03239318ce6b7
https://github.com/AdityaKuntal/backen-nodejs.git <akjaat001kuntal@gmail.com>
2022-10-25 12:46:23 +0530

    update all chnages

--- src/authorController/authorController.js
+++ src/authorController/authorController.js
@@ -1,17 +1,21 @@
  const bookModel = require('../bookModel/bookModel')
+//  const{isValidRequestBody,isValid,isValidName, isNaNYear}=require('../validator/validator')
 
 
-
+ 
 const createAuthor = async function (req,res){
-    let newAuthor = req.body
-    let Author = await bookModel.create(newAuthor)
+    let requestBody = req.body
+
+    let Author = await bookModel.create(requestBody)    
     res.send({data:Author , status:true})
 }
 
 const getAuthorDetails = async function(req ,res){
-    let newDetalis = await bookModel.find()
+    
+let newDetalis =  await bookModel.find()
     res.send({data:newDetalis , status:true})
 }
+
 
 module.exports.createAuthor=createAuthor
 module.exports.getAuthor=getAuthorDetails
--- src/bookModel/bookModel.js
+++ src/bookModel/bookModel.js
@@ -1,22 +1,30 @@
 const mongoose = require('mongoose')
+// const xyz = require ('../validator/validator')
 
 
 
 let bookSchema = new mongoose.Schema({
  
-    bookName: String,
-    authorName: String,
-    category: String,
-    year: Number
+bookName: {
+    type:String,
+    require: [true, "bookName name is required"],
+    unique : true,
+  
+},                           
+authorName:{
+        type:String,
+        require:[true, "authorName name is required"],
+    
+    },
+category:{
+        type:[String],
+        require:[true, "category name is required"]
+    },
 
-
-})
-
-
-
-
-
-
-
+year: {
+        type:Number,
+        require:[true, "year name is required"]
+    }
+      },{ timestamps: true })
 module.exports = mongoose.model('Book Model',bookSchema)
 
