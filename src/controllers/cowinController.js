11c72b82c4ce6c5407c0b325f290415ee927679d
soniya437 <ysoniya437@gmail.com>
2022-11-11 00:09:13 +0530

    assignment

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -38,6 +38,25 @@ let getDistricts = async function (req, res) {
     }
 }
 
+let getDistrictById = async function (req, res) {
+    try {
+        let district = req.query.districtId
+        let date = req.query.date
+        let options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district}&date=${date}`
+        }
+        let result = await axios(options);
+        console.log(result)
+        let data = result.data
+        res.status(200).send({ msg: data, status: true })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
 let getByPin = async function (req, res) {
     try {
         let pin = req.query.pincode
@@ -83,3 +102,4 @@ module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getDistrictById =getDistrictById
--- src/routes/route.js
+++ src/routes/route.js
@@ -4,16 +4,12 @@ const CowinController= require("../controllers/cowinController")
 
 
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-
 router.get("/cowin/states", CowinController.getStates)
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
+router.get("/cowin/district", CowinController.getDistrictById)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
