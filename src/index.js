dfe3aa24ffdea6bd4a35b8e661ce9da3d188c03a
soniya437 <ysoniya437@gmail.com>
2022-11-11 00:05:03 +0530

    weather assignment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://soni-45:cU9ozYi01hcRz2Ne@mongo2.ncvkeai.mongodb.net/Axios-dbtest", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
