6da3a8e93e553e154cf9edce5d2cf6569309a420
exactshubham <exactshubham@gmail.com>
2022-11-15 09:56:36 +0530

    modified

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,6 +1,7 @@
 const blogModel = require('../models/blogsModel')
 const mongoose = require('mongoose')
 const objectId = mongoose.isValidObjectId
+const validator = require("../util/validator");
 let createBlog = async (req, res) => {
 
     try {
@@ -35,80 +36,81 @@ let createBlog = async (req, res) => {
 
 
 const getBlogs = async function (req, res) {
+  try {
+    let filterQuery = { isDeleted: false, deletedAt: null, isPublished: true };
+    let queryParams = req.query;
+    const { authorId, category, tags, subcategory } = queryParams;
 
-    try {
-        const query = req.query
-        let { authorId, category, subcategory, tags } = query
-
-        const savedData = await blogModel.find({ isPublished: true, isDeleted: false })
-        if (!savedData.length > 0) {
-            return res.status(404).send({ status: false, msg: "No details found" })
-        }
-        if (!query) {
-            return res.status(200).send({ status: true, data: savedData })
-        }
-        const filter = {}
-        if (authorId) {
-
-            if (!objectId(authorId)) {
-                return res.status(400).send({ status: false, msg: "Enter valid authorId" })
-            }
-            filter.authorId = authorId
-        }
-        if (category) {
-            if (!category.match(/([A-Z,a-z])\w+/g)) {
-                return res.status(400).send({ status: false, msg: "Enter valid category" })
-            }
-            filter.category = category
-        }
-
-        if (tags) {
-            try {
-                tags = JSON.parse(tags)
-            }
-            catch (error) {
-                return res.status(400).send({ status: false, msg: "Please enter tags in array" })
-            }
-            for (let i = 0; i < tags.length; i++) {
-                // if (typeof (tags[i]) !== String) {
-                //     return res.status(400).send({ status: false, msg: "Please enter response in string" })
-                // }
-                if (!tags[i].match(/([A-Z,a-z])\w+/g)) {
-                    return res.status(400).send({ status: false, msg: "Enter valid category" })
-                }
-            }
-            filter.tags = tags
-        }
-
-
-        if (subcategory) {
-            try {
-                subcategory = JSON.parse(subcategory)
-            }
-            catch (error) {
-                return res.status(400).send({ status: false, msg: "Please enter subcategory in array" })
-            }
-            for (let i = 0; i < subcategory.length; i++) {
-                // if (typeof (subcategory[i]) !== String) {
-                //     return res.status(400).send({ status: false, msg: "Please enter response in string" })
-                //  }
-                if (!subcategory[i].match(/([A-Z,a-z])\w+/g)) {
-                    return res.status(400).send({ status: false, msg: "Enter valid subcategory" })
-                }
-            }
-            filter.subcategory = subcategory
-        }
-
-        let savedData1 = await blogModel.find(filter)
-        if(!savedData1.length > 0){
-            return res.status(400).send({status: false, msg: "No such data"})
-        }
-        return res.send({ status: true, data: savedData1 })
-
-    } catch (error) {
-        return res.status(500).send({ status: false, msg: error.message })
+    if (!validator.isValidString(authorId)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Author id is required" });
     }
-}
+    if (authorId) {
+      if (!validator.isValidObjectId(authorId)) {
+        return res.status(400).send({
+          status: false,
+          message: `authorId is not valid.`,
+        });
+      }
+    }
+
+    if (!validator.isValidString(category)) {
+      return res.status(400).send({
+        status: false,
+        message: "Category cannot be empty while fetching.",
+      });
+    }
+
+    if (!validator.isValidString(tags)) {
+      return res.status(400).send({
+        status: false,
+        message: "tags cannot be empty while fetching.",
+      });
+    }
+    if (!validator.isValidString(subcategory)) {
+      return res.status(400).send({
+        status: false,
+        message: "subcategory cannot be empty while fetching.",
+      });
+    }
+
+    if (validator.isValidRequestBody(queryParams)) {
+      const { authorId, category, tags, subcategory } = queryParams;
+      if (validator.isValid(authorId) && validator.isValidObjectId(authorId)) {
+        filterQuery["authorId"] = authorId;
+      }
+      if (validator.isValid(category)) {
+        filterQuery["category"] = category.trim();
+      }
+      if (validator.isValid(tags)) {
+        const tagsArr = tags
+          .trim()
+          .split(",")
+          .map((x) => x.trim());
+        filterQuery["tags"] = { $all: tagsArr };
+      }
+      if (validator.isValid(subcategory)) {
+        const subcatArr = subcategory 
+          .trim()
+          .split(",")
+          .map((subcat) => subcat.trim());
+        filterQuery["subcategory"] = { $all: subcatArr };
+      }
+    }
+    const blog = await blogModel.find(filterQuery);
+    console.log(blog)
+
+    if (Array.isArray(blog) && blog.length === 0) {
+      return res.status(404).send({ status: false, message: "No blogs found" });
+    }
+    res.status(200).send({ status: true, message: "Blogs list", data: blog });
+  } catch (error) {
+    res.status(500).send({ status: false, Error: error.message });
+  }
+};
+
+
 
 
 module.exports.createBlog = createBlog
