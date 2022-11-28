82f2094312d8b5de70a61adbe57ead789c8d4ea3
soniya437 <ysoniya437@gmail.com>
2022-11-28 15:01:19 +0530

    user register

---
+++ src/controller/userController.js
@@ -0,0 +1,43 @@
+const userModel = require('../model/userModel');
+
+    let nameValidation = (/^[a-zA-Z]+([\s][a-zA-Z]+)*$/.test(name));
+    const validateEmail = (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email));
+    const validatePassword = (/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,15}$/.test(password))
+    const validatePhone = ((/^(\+\d{1,3}[- ]?)?\d{10}$/.test(phone)))
+
+const createUser = async function  (req, res) {
+    try {
+        let data = req.body
+        let { title, name, email, phone, password } = data
+
+        
+        if (!title) return res.status(400).send({ status: false, msg: "title is mandatory" })
+        if  ((title !== "Mr" && title !== "Mrs" && title !== "Miss")) return res.status(400).send({ status: false, msg: "give title only ['Mr'/ 'Mrs'/'Miss']" });
+
+        if (!name || !email || !phone || !password ) return res.status(400).send({ status: false, msg: "Mandatory fields are required" })
+        
+       
+
+        if (typeof name !== "string"|| !nameValidation) 
+        return res.status(400).send({ status: false, msg: "please enter a valid name" })
+
+        if (!validatePhone) return res.status(400).send({ status: false, msg: "Please enter valid Phone Number" })
+        if (!validateEmail) return res.status(400).send({ status: false, msg: "Email is invalid, Please check your Email address" });
+        if (!validatePassword) return res.status(400).send({ status: false, msg: "use a strong password at least =>  one special, one Uppercase, one lowercase (character) one numericValue and password must be eight characters or longer)" });
+
+       
+        let findnumber = await userModel.find({ phone: phone })
+
+        let findemail = await userModel.find({ email: email })
+
+        if (findnumber.length > 0) return res.status(400).send({ status: false, msg: "mobile no. is already exist" })
+        if (findemail.length > 0) return res.status(400).send({ status: false, msg: "email id is already exist" })
+
+            let saveData = await userModel.create(data)
+            res.status(201).send({  status: true, msg: saveData  })
+    } catch (error) {
+        res.status(500).send({ status: false, msg: error.message });
+    }
+}
+
+module.exports.createUser =createUser
--- src/router/router.js
+++ src/router/router.js
@@ -1,40 +1,9 @@
 const express = require("express")
 const router = express.Router()
-
-const bookModel = require("../model/bookModel")
-
-router.get("/test-me" , function (req,res){
+const userController = require('../controller/userController')
 
 
-    // let regex = /^[a-z]*$/gm
-
-    // // const clgNmaeRegex = /^([a-z]{2,})*$/gm
-
-
-    // let name = "aaaA"
-
-    // // let mat = name.match(name)
-    // let mat = regex.test(name)
-
-    // console.log(mat)
-
-
-    // const isValid = function (value) {
-    //     if (typeof value === "undefined" || value === null) return false;
-    
-    //     if (typeof value === 'string' && value.trim().length === 0) return false
-    
-    //     return true;
-    // }
-
-
-    // let mat = isValid("null")
-
-
-    res.status(200).send("Connection Done!")
-})
-
-
+router.post("register" , userController.createUser)
 
 
 
