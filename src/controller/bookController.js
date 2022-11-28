a03516568ab34229cd61e71aa88966a962d70018
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2022-11-28 22:02:47 +0530

    Update in Get book and make comment on regex ISBN abd excerpt

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose')
 const objectId = mongoose.Types.ObjectId
 
 
-const  isbnRegex =   /^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/
+const  isbnRegex =   /^(?=(?:\D*\d){10,}(?:(?:\D*\d){3})?$)[\d-]+$/
 const excerptRegex = /^([A-Za-z ]+){3,}$/
 
 
@@ -31,16 +31,16 @@ const createBook = async function (req, res) {
         if(!isValidEntry(userId) || !objectId.isValid(userId)) return res.status(400).send({status : false , message : "UserId is not given or Invalid. "})
         if(!isValidEntry(category)) return res.status(400).send({status : false , message : "Category is not given."})
         if(!isValidEntry(subcategory)) return res.status(400).send({status : false , message : "Subcategory is not given."})
-        if(!isValidEntry(ISBN) || isbnRegex.test(ISBN)) return res.status(400).send({status : false , message : "ISBN is not given or Not Invalid."})
-        if(!isValidEntry(excerpt) || excerptRegex.test(excerpt)) return res.status(400).send({status : false , message : "Excerpt is not given or Not Invalid."})
+        
+        // if(! excerpt.match(excerptRegex)) return res.status(400).send({status : false , message : "Excerpt is not given or Not Invalid."})
+        // if(!isValidEntry(ISBN) || isbnRegex.test(ISBN)) return res.status(400).send({status : false , message : "ISBN is not given or Not Invalid."})
 
-
-        let allreadyBook = bookModel.findOne({$or : [{title : title} , {ISBN : ISBN}]})
-        if(allreadyBook) return res.status(400).send({status: false, message: "Title of book or ISBN of book , already present."})
+        let allreadyBook = bookModel.find({$or : [ {title : data.title} , {ISBN : data.ISBN} ]})
+        if(allreadyBook >= 0) return res.status(400).send({status: false, message: "Title of book or ISBN of book , already present."})
 
 
         let userCheck = userModel.findOne({userId : userId})
-        if(! userCheck) returnres.status(400).send({status: false, message: "Invalid UserId , user not exist in DB"})
+        if(!userCheck) returnres.status(400).send({status: false, message: "Invalid UserId , user not exist in DB"})
 
 
         const today = moment()        
@@ -70,9 +70,11 @@ const getBooks = async function (req, res) {
 
         let findObj = { isDeleted: false, ...query }
 
-        let allBook = await bookModel.find(findObj).sort({ title: 1 }).select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1,  reviews: 1 , releasedAt : 1 })
+        
+        let allBook = await bookModel.find(findObj).select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1,  reviews: 1 , releasedAt : 1 }).sort({ title: 1 })
+        
+        if (allBook.length <= 0) return res.status(404).send({ status: false, message: "No book found (With given query)" })
 
-        if (allBook.length === 0) return res.status(404).status({ status: false, message: "No book found (With given query)" })
 
         res.status(200).send({
             status: true,
